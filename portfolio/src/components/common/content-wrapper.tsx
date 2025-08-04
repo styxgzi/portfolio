4456ac9164b53b6432524fa665ddc0ebf72d11aa
shubh <styxgzi@gmail.com>
2025-08-05 02:27:23 +0530

    Delete portfolio directory

--- portfolio/src/components/common/content-wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ContentWrapper({
-  children,
-}: {
-  children: React.ReactElement;
-}) {
-  return <div className="max-w-[1800px] mx-auto">{children}</div>;
-}
